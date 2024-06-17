10dfcd1a32ace3c64b61a496049c2e2741606ac3
Christopher Bellocci <christopher.bellocci@hotmail.com>
2024-06-17 14:30:11 +0100

    hopefully fixed user issue

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -74,12 +74,12 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name,
+      firstName: first_name, 
       lastName: last_name,
       photo: image_url,
     };
 
-    const newUser = await createUser(user);
+    const newUser = await createUser(user as any);
 
     // Set public metadata
     if (newUser) {
@@ -104,7 +104,7 @@ export async function POST(req: Request) {
       photo: image_url,
     };
 
-    const updatedUser = await updateUser(id, user);
+    const updatedUser = await updateUser(id, user as any);
 
     return NextResponse.json({ message: "OK", user: updatedUser });
   }
